669e184d87e1fa0b68eca01eafaa47d03701c850
The-vinicius <viniciusnao41@gmail.com>
2022-08-02 16:04:23 +0000

    add urls

--- quickstart/urls.py
+++ quickstart/urls.py
@@ -1,7 +1,10 @@
 from rest_framework import routers
-from .views import hello_world
+from .views import hello_world, CreateAluno, CreateEscola
 from django.urls import path
 
 urlpatterns = [
-    path('hello/', hello_world, name='hello')
+    path('escola/', CreateEscola.as_view(), name='soll'),
+    path('hello/', hello_world, name='hello'),
+    path('aluno/', CreateAluno.as_view(), name='create'),
+    path('alunos/<int:pk>', CreateAluno.as_view(), name='update'),
 ]
