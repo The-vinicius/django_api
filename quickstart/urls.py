bfc070cacd72e4188f5751b1abae0334e1ca6d39
The-vinicius <viniciusnao41@gmail.com>
2022-09-10 10:02:03 +0000

    added urls sample and cache

--- quickstart/urls.py
+++ quickstart/urls.py
@@ -1,10 +1,12 @@
 from rest_framework import routers
-from .views import hello_world, CreateAluno, CreateEscola
+from .views import hello_world, CreateAluno, CreateEscola, hello_cache, sample
 from django.urls import path
 
 urlpatterns = [
     path('escola/', CreateEscola.as_view(), name='soll'),
     path('hello/', hello_world, name='hello'),
+    path('sample', sample, name='sample'),
+    path('cache/', hello_cache, name='cache'),
     path('aluno/', CreateAluno.as_view(), name='create'),
     path('alunos/<int:pk>', CreateAluno.as_view(), name='update'),
 ]
